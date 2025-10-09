0cd6a0fc9f22dea8aaa91d19efc97717dcdfb752
babaygo <simon.laurent@epsi.fr>
2025-10-09 17:32:48 +0200

    Add type Product in front + add weight and dimensions for a product

---
+++ frontend/src/types/Product.ts
@@ -0,0 +1,19 @@
+import { Category } from "./Category";
+
+export interface Product {
+    id: number;
+    name: String;
+    slug: String;
+    description: String;
+    price: number;
+    stock: number;
+    weight: number;
+    height: number;
+    lenght: number;
+    width: number;
+    images: string[];
+    categoryId: number;
+    category: Category;
+    createdAt: Date;
+    updatedAt: Date;
+}
