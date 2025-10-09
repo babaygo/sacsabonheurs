8d043d337b5ddbfa654dadc7e7deaae23b9855e8
babaygo <simon.laurent@epsi.fr>
2025-10-10 01:12:57 +0200

    Add infos on product

--- frontend/src/app/products/[slug]/page.tsx
+++ frontend/src/app/products/[slug]/page.tsx
@@ -6,6 +6,7 @@ import {
     AccordionItem,
     AccordionTrigger,
 } from "@/components/ui/accordion"
+import { Product } from "@/types/Product";
 
 async function getProduct(slug: string) {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products/${slug}`);
@@ -14,13 +15,13 @@ async function getProduct(slug: string) {
 }
 
 export default async function ProductPage({ params }: { params: { slug: string } }) {
-    const product = await getProduct(params.slug);
+    const product: Product = await getProduct(params.slug);
     if (!product) notFound();
 
     const images = JSON.parse(product.images);
 
     return (
-        <div className="max-w-7xl mx-auto p-4 mt-8">
+        <div className="max-w-7xl mx-auto min-h-screen p-4 mt-8">
             <div className="grid grid-cols-2 gap-8">
                 <div className="grid grid-cols-2 gap-4">
                     {images.map((src: string, i: number) => (
@@ -34,11 +35,23 @@ export default async function ProductPage({ params }: { params: { slug: string }
                     <AddToCartDrawer product={product} />
 
                     <div className="flex flex-col">
-                        <Accordion type="single" collapsible>
+                        <Accordion type="multiple">
                             <AccordionItem value="item-1">
                                 <AccordionTrigger>Description</AccordionTrigger>
                                 <AccordionContent>
                                     {product.description}
+                                </AccordionContent>
+                            </AccordionItem>
+                            <AccordionItem value="item-2">
+                                <AccordionTrigger>Dimensions</AccordionTrigger>
+                                <AccordionContent>
+                                    {product.height}*{product.lenght}*{product.width}*
+                                </AccordionContent>
+                            </AccordionItem>
+                            <AccordionItem value="item-3">
+                                <AccordionTrigger>Poids</AccordionTrigger>
+                                <AccordionContent>
+                                    {product.weight}g
                                 </AccordionContent>
                             </AccordionItem>
                         </Accordion>
