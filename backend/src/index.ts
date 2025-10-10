0c87b05be5bba0fdfa1d52b43805c8b9aacf8898
babaygo <simon.laurent@epsi.fr>
2025-10-10 15:01:29 +0200

    Add config deploy backend on render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -548,4 +548,5 @@ app.get("/api/admin/orders/:id", requireAuth, async (req, res) => {
     }
 });
 
-app.listen(3001, () => console.log('API running on port 3001'));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Serveur lancé sur le port ${PORT}`));
