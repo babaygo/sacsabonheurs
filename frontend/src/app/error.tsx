ba2760f9e8e3a3f8f8148562287c792d1c9285c0
babaygo <simon.laurent@epsi.fr>
2025-11-04 01:35:52 +0100

    rework message error

--- frontend/src/app/error.tsx
+++ frontend/src/app/error.tsx
@@ -1,17 +1,10 @@
 "use client";
 
-export default function Error({
-    error,
-    reset,
-}: {
-    error: Error & { digest?: string }
-    reset: () => void
-}) {
+export default function Error() {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center text-center">
             <h1 className="text-2xl font-bold text-gray-800 mb-4">Erreur système !</h1>
-            <div>{error.message}</div>
-            <button onClick={() => reset()}>Try again</button>
+            <p>Une erreur s'est produite, veuillez réessayer plus tard.</p>
         </div>
     )
 }
