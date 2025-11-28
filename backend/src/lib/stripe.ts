f96586b5deeba8bae6197cef9dd585cdd9a86e07
babaygo <simon.laurent@epsi.fr>
2025-11-28 16:37:07 +0100

    fix: use absolute paths to src/generated for ESM imports

--- backend/src/lib/stripe.ts
+++ backend/src/lib/stripe.ts
@@ -1,5 +1,5 @@
 import Stripe from "stripe";
-import { User } from "../generated/prisma/index.js";
+import { User } from "../../src/generated/prisma/index.js";
 
 const stripe =  new Stripe(process.env.STRIPE_SECRET_KEY!);
 
