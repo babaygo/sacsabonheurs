3a9f3cbd19370beeea5186f927a43dcda8faeea7
babaygo <simon.laurent@epsi.fr>
2026-02-10 18:14:30 +0100

    Add config auto sitemap and robots.txt

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -1,9 +1,25 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: 'https://sacsabonheurs.fr',
-    generateRobotsTxt: false,
+    generateRobotsTxt: true,
     sitemapSize: 5000,
     changefreq: 'weekly',
     priority: 0.7,
     exclude: ['/admin*', '/choose-relay', '/signup', '/login', '/reset-password', '/orders'],
+    robotsTxtOptions: {
+        sitemaps: [
+            'https://sacsabonheurs.fr/sitemap.xml',
+        ],
+        policies: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: ['/admin', '/orders', '/api/*'],
+            },
+        ],
+    },
+    additionalSitemaps: [
+        'https://sacsabonheurs.fr/sitemap-products.xml',
+        'https://sacsabonheurs.fr/sitemap-blog.xml',
+    ],
 };
